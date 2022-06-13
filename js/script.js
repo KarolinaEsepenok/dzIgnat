239fa0875e7bf8fde1b7a1a97bae6b50e1f1cb85
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-06-13 21:26:57 +0300

    lessh4 commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+
+// accordion
+
+document.querySelector('#plus1').onclick = function(){
+    document.querySelector('#info1').classList.toggle('answer-hide');
+    document.querySelector('#plus1').classList.toggle('close');
+}
+
+document.querySelector('#plus2').onclick = function(){
+    document.querySelector('#info2').classList.toggle('answer-hide');
+    document.querySelector('#plus2').classList.toggle('close');
+}
+
+document.querySelector('#plus3').onclick = function(){
+    document.querySelector('#info3').classList.toggle('answer-hide');
+    document.querySelector('#plus3').classList.toggle('close');
+}
+document.querySelector('#plus4').onclick = function(){
+    document.querySelector('#info4').classList.toggle('answer-hide');
+    document.querySelector('#plus4').classList.toggle('close');
+}
+document.querySelector('#plus5').onclick = function(){
+    document.querySelector('#info5').classList.toggle('answer-hide');
+    document.querySelector('#plus5').classList.toggle('close');
+}
+document.querySelector('#plus6').onclick = function(){
+    document.querySelector('#info6').classList.toggle('answer-hide');
+    document.querySelector('#plus6').classList.toggle('close');
+}
