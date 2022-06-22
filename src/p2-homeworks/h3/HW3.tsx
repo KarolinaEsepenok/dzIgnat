0cf1c8acd4807798feea5623ba84965a0561c625
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-06-22 22:57:19 +0300

    second commit

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -24,8 +24,7 @@ function HW3() {
     return (
         <div>
             <hr/>
-            homeworks 3
-
+           
             {/*should work (должно работать)*/}
             <GreetingContainer users={users} addUserCallback={addUserCallback}/>
 
