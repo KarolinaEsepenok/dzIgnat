52e40fbc40063043c423e11741dfa2fe1efce204
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-07-24 17:17:13 +0300

    Updates

---
+++ precache-manifest.155bd058b9b76c3deaa109a075aaadeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d29c25704e1c147726fc95da748ec33",
+    "url": "/dzIgnat/index.html"
+  },
+  {
+    "revision": "01205349a2f19afd3f6d",
+    "url": "/dzIgnat/static/css/main.1ce58e2d.chunk.css"
+  },
+  {
+    "revision": "9e4d72c10fdc635bbd85",
+    "url": "/dzIgnat/static/js/2.59f8beaa.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/dzIgnat/static/js/2.59f8beaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01205349a2f19afd3f6d",
+    "url": "/dzIgnat/static/js/main.225d361b.chunk.js"
+  },
+  {
+    "revision": "abd7c12d46323575aa99",
+    "url": "/dzIgnat/static/js/runtime-main.1bdde955.js"
+  },
+  {
+    "revision": "3926c099f64ff257a313f4221f5e5b05",
+    "url": "/dzIgnat/static/media/checkbox-png.3926c099.png"
+  }
+]);
