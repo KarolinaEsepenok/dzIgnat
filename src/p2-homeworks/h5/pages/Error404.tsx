3e4e0892f75e8e56913ebebc97689a46c29c9eb6
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-07-25 21:10:36 +0300

    reactDomCommit

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -5,7 +5,7 @@ function Error404() {
         <div>
             <div>404</div>
             <div>Page not found!</div>
-            <div>——</div>
+            <div>—hhhh—</div>
         </div>
     )
 }
